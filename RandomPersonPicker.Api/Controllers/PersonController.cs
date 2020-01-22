98dfb3df2136c7d24e29cdd2c22e38ddc30179c2
Nicholas Gowdy <nicholasg@nandos-ltmb-046.local>
2020-01-22 12:48:17 +0000

    Added web api project.

---
+++ RandomPersonPicker.Api/Controllers/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomPersonPicker.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+      
+    }
+}
