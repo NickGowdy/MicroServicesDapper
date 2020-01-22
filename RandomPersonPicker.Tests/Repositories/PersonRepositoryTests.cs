498a8ef2a7184fe7bd31b6bea56a7401a8b2089a
Nicholas Gowdy <nicholasg@nandos-ltmb-046.local>
2020-01-22 10:37:55 +0000

    Added unit test for repo.

---
+++ RandomPersonPicker.Tests/Repositories/PersonRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Transactions;
+using RandomPersonPicker.Data.Repositories;
+using RandomPersonPicker.Domain.Models;
+using Xunit;
+
+namespace RandomPersonPicker.Tests.Repositories
+{
+    public class PersonRepositoryTests
+    {
+        [Theory]
+        [InlineData("Nick", "Gowdy")]
+        public async Task GetPerson(string forename, string surname)
+        {
+            using (var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                // ARRANGE
+                var personRepository = new PersonRepository();
+                var newPersonId = await personRepository.Insert(new Person { Forename = forename, Surname = surname });
+
+                // ACT
+                var person = await personRepository.Get((int)newPersonId);
+
+                // ASSERT
+                Assert.NotNull(person);
+                Assert.Equal(newPersonId, person.PersonId);
+                Assert.Equal(forename, person.Forename);
+                Assert.Equal(surname, person.Surname);
+            }
+        }
+    }
+}
