f903b30c691cc9e36b84156bbb71b75d3a4b4011
Nicholas Gowdy <nicholasg@nandos-ltmb-046.local>
2020-01-27 10:29:53 +0000

    Added endpoint to get random person.

--- RandomPersonPicker.Api/Controllers/PersonController.cs
+++ RandomPersonPicker.Api/Controllers/PersonController.cs
@@ -3,11 +3,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RandomPersonPicker.Data.Interfaces.Repositories;
 using RandomPersonPicker.Domain.Models;
+using System;
+using System.Linq;
 
 namespace RandomPersonPicker.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Person")]
     public class PersonController
     {
         private readonly IPersonRepository _personRepository;
@@ -22,6 +24,17 @@ namespace RandomPersonPicker.Api.Controllers
         {
             var People = await _personRepository.Get();
             return People;
+        }
+
+        [HttpGet]
+        [Route("Random")]
+        public async Task<Person> Random()
+        {
+            var People = await _personRepository.Get();
+            var PeopleIds = People.Select(x => x.PersonId);
+            var Random = new Random();
+            var RandomPersonId = Random.Next(PeopleIds.Min(), PeopleIds.Max());
+            return People.FirstOrDefault(x => x.PersonId == RandomPersonId);
         }
     }
 }
